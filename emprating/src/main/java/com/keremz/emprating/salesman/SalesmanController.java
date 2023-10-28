0c02127167dcca1451445a03272dae195a982bd7
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-10-28 23:26:37 +0200

    feat: create salesman controller

---
+++ emprating/src/main/java/com/keremz/emprating/salesman/SalesmanController.java
@@ -0,0 +1,35 @@
+package com.keremz.emprating.salesman;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class SalesmanController {
+
+    @Autowired
+    private SalesmanService salesmanService;
+
+    @PostMapping("/salesmen/")
+    public ResponseEntity<Salesman> create(@RequestBody Salesman salesman) {
+        Salesman s = Salesman.builder()
+                .employeeId(salesman.getEmployeeId())
+                .name(salesman.getName())
+                .department(salesman.getDepartment())
+                .build();
+        return new ResponseEntity<>(salesmanService.create(s), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/salesmen/{employeeId}/")
+    public ResponseEntity<Salesman> getSalesman(@PathVariable Integer employeeId) {
+        return new ResponseEntity<>(salesmanService.getSalesman(employeeId), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/salesmen/{employeeId}/")
+    public ResponseEntity<String> deleteSalesman(@PathVariable Integer employeeId) {
+        salesmanService.deleteSalesman(employeeId);
+        return new ResponseEntity<>("Salesman with employeeID: " + employeeId + " successfully deleted", HttpStatus.OK);
+    }
+}
