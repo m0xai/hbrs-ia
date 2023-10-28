5d136fefa1561479eb4c824d57b5b8bcce3a30a0
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-10-28 23:26:46 +0200

    feat: create salesman service

---
+++ emprating/src/main/java/com/keremz/emprating/salesman/SalesmanService.java
@@ -0,0 +1,33 @@
+package com.keremz.emprating.salesman;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SalesmanService {
+
+    @Autowired
+    private SalesmanRepository salesmanRepository;
+
+    public Salesman create(Salesman salesman) {
+        if (salesmanRepository.existsByEmployeeId(salesman.employeeId)) {
+            // TODO: Create custom Salesman Exceptions
+            throw new RuntimeException("Fehler: Salesman already exists.");
+        }
+        return salesmanRepository.save(salesman);
+    }
+
+    public Salesman getSalesman(Integer employeeId) {
+        if (!salesmanRepository.existsByEmployeeId(employeeId)) {
+            throw new RuntimeException("Salesman with employeeId: " + employeeId + " not found.");
+        }
+        return salesmanRepository.findByEmployeeId(employeeId);
+    }
+
+    public void deleteSalesman(Integer employeeId) {
+        if (!salesmanRepository.existsByEmployeeId(employeeId)) {
+            throw new RuntimeException("Salesman with employeeId: " + employeeId + " not found.");
+        }
+        salesmanRepository.deleteByEmployeeId(employeeId);
+    }
+}
