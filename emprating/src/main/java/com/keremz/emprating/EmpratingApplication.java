75911baef291fad9e1093cdd64621cd782d8d40e
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-10-28 22:45:02 +0200

    feat: init emparating backend app

---
+++ emprating/src/main/java/com/keremz/emprating/EmpratingApplication.java
@@ -0,0 +1,12 @@
+package com.keremz.emprating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpratingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpratingApplication.class, args);
+	}
+}
---
+++ emprating/src/test/java/com/keremz/emprating/EmpratingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keremz.emprating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpratingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
